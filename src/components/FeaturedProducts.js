1011efa398260e4f9352c8c560ac07232d4b799d
Desmond Williams <demowi8@gmail.com>
2022-08-01 07:35:42 -0400

    featured products error and filtered 3

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -14,8 +14,20 @@ const FeaturedProducts = () => {
     if (loading) {
       return <Loading />
     }
-
-    return <h4>featured products</h4>
+    if (error) {
+      return <Error />
+    }
+    return <Wrapper className='section'>
+      <div className='title'>
+        <h2>Featured Products</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='section-center featured'>
+        {featured.slice(0,3).map((product)=>{
+          return <Product key={product.id} {...product} />
+        })}
+      </div>
+    </Wrapper>
 }
 
 const Wrapper = styled.section`
