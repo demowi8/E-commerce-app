7d16fe5ae7a23b8a37df57032e92703db8e35c36
Desmond Williams <demowi8@gmail.com>
2022-08-01 01:12:23 -0400

    page index configuration

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import HomePage from './HomePage.js'
+import ProductsPage from './ProductsPage.js'
+import SingleProductPage from './SingleProductPage.js'
+import AboutPage from './AboutPage.js'
+import CartPage from './CartPage.js'
+import ErrorPage from './ErrorPage.js'
+import CheckoutPage from './CheckoutPage.js'
+import PrivateRoute from './PrivateRoute.js'
+
+export {
+    HomePage,
+    ProductsPage,
+    SingleProductPage,
+    AboutPage,
+    CartPage,
+    ErrorPage,
+    CheckoutPage,
+    PrivateRoute,
+}
