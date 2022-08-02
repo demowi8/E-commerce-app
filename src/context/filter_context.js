02ad650492d7e1c80077685c8c25ee02ef301f6b
Desmond Williams <demowi8@gmail.com>
2022-08-02 19:11:04 -0400

    all filters set up and search filter functionality

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -68,14 +68,23 @@ export const FilterProvider = ({ children }) => {
     if (name=== 'category') {
       value=e.target.textContent
     }
+    if(name === 'color'){
+      value = e.target.dataset.color
+    }
+    if(name === 'price'){
+      value = Number(value)
+    }
+    if(name === 'shipping'){
+      value = e.target.checked
+    }
     dispatch({type: UPDATE_FILTERS, payload:{name,value}})
   }
   const clearFilters = () => {
-
+    dispatch({type:CLEAR_FILTERS})
   }
 
   return (
-    <FilterContext.Provider value={{...state, setGridView, setListView, updateSort, updateFilters, clearFilters}}>
+    <FilterContext.Provider value={{...state, setGridView, setListView, updateSort, updateFilters, clearFilters,}}>
       {children}
     </FilterContext.Provider>
   )
