c712c30ee29a07b109104f2a895bf9923f88f8e4
Desmond Williams <demowi8@gmail.com>
2022-08-02 23:44:43 -0400

    add to cart reducer function exisiting item

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -29,6 +29,15 @@ export const CartProvider = ({ children }) => {
       product
     }})
   }
+  //remove item
+  const removeItem = (id) => {}
+
+  //toggle amount
+  const toggleAmount = (id,value) => {}
+
+  //clear cart
+  const clearCart = () => {}
+  
   return (
     <CartContext.Provider value={{ ...state, addToCart }}>
       {children}
